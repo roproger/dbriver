f628beab049eebd516e88537f646938ac7f34d7d
Ростислав Профит <rostislavprofit@MacBook-Pro-Rostislav.local>
2021-05-21 20:32:40 +0300

    0.1.7 Add groupBy part and testing

--- test/index.js
+++ test/index.js
@@ -10,27 +10,7 @@ const db = createConnector({
 db.connect().then(async function () {
   console.log("Connected")
   console.log(
-    db
-      .select()
-      .from("user")
-      .where({
-        id: 5,
-        status: {
-          $in: [
-            "manager",
-            "it",
-            db.select("name").from("statuses").where({ id: 5 }),
-          ],
-        },
-        age: { $btw: [18, 40] },
-        name: { $lk: "Stephan" },
-        taskList: { $isnt: null },
-        $or: {
-          rate: { $gt: 3 },
-          joinDate: { $lt: new Date("2021-02-15") },
-        },
-      })
-      .toSqlString()
+    db.select().from("user").groupByWithRollup("name", 2).toSqlString()
   )
-  // select * from `user` where `id`=5 and `status` in ('manager','it',(select `name` from `statuses` where `id`=5)) and `age` between 18 and 40 and `name` like 'Stephan' and `taskList` is not NULL and (`rate`>3 and `joinDate`<'2021-02-15 02:00:00.000')
+  // select * from `user` group by `name`,2 with rollup
 })
