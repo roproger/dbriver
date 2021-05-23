0d411b04922447eb0fd8da03685bb0b27466d871
Ростислав Профит <rostislavprofit@MacBook-Pro-Rostislav.local>
2021-05-23 20:19:21 +0300

    0.2.3 Add insert part and testing

--- test/index.js
+++ test/index.js
@@ -9,6 +9,10 @@ const db = createConnector({
 
 db.connect().then(async function () {
   console.log("Connected")
-  const query = db.insert().flag("LOW_PRIORITY")
+  const query = db.insert(
+    ["test", 123, { $: "CURRENT_TIME" }, db.select("id").from("user").limit(1)],
+    [new Date(), { $eId: "test2" }, null]
+  )
   console.log(query.toSqlString())
+  // insert values ('test',123,CURRENT_TIME,(select `id` from `user` limit 1)),('2021-05-23 20:14:15.800',`test2`,NULL,NULL)
 })
