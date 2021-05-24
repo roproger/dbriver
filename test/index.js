482bda99da3ab72bf51a0ea8d32756d5ac490d0d
Ростислав Профит <rostislavprofit@MacBook-Pro-Rostislav.local>
2021-05-24 21:43:54 +0300

    0.4.0 Update ext

--- test/index.js
+++ test/index.js
@@ -10,12 +10,14 @@ const db = createConnector({
 db.connect().then(async function () {
   console.log("Connected")
   const query = db
-    .update("test")
-    .set({ a: { $eId: "b" } })
-    .set({ c: 1 })
-    .where({ id: 5 })
-    .orderBy({ a: "desc" })
-    .limit(10)
+    .update(
+      { t: "test" },
+      { join: { t2: "test2" }, prefix: "left", on: { "t2.a_id": "t.id" } }
+    )
+    .set({
+      a: db.select("a").from("b").limit(1),
+    })
+    .orderBy([1, "asc"], [2, "desc"])
   console.log(query.toSqlString())
-  // update `test` set `a`=`b`,`c`=1 where `id`=5 order by `a` desc limit 10
+  // update `test` `t` left join `test2` `t2` on `t2`.`a_id`='t.id' set `a`=(select `a` from `b` limit 1) order by 1 asc,2 desc
 })
