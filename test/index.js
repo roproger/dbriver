86278223bdf825ae21fff3528508140099b508c5
Ростислав Профит <rostislavprofit@MacBook-Pro-Rostislav.local>
2021-05-24 21:29:16 +0300

    0.3.2 Add flag part

--- test/index.js
+++ test/index.js
@@ -9,11 +9,7 @@ const db = createConnector({
 
 db.connect().then(async function () {
   console.log("Connected")
-  const query = db
-    .insert("(1,2,3)", { row: ["test"] })
-    .cols("a", "b", "c")
-    .insert({ row: { c: "test'string", a: db.select("a").from("d").limit(1) } })
-    .into("table")
+  const query = db.update().flag("IGNORE")
   console.log(query.toSqlString())
-  // insert into `table` (`a`,`b`,`c`) values (1,2,3),row('test',NULL,NULL),row((select `a` from `d` limit 1),NULL,'test\'string')
+  // update IGNORE
 })
