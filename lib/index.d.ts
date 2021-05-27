1c0a7e891ac694d5bfb4758a28341cf04080c360
Ростислав Профит <rostislavprofit@MacBook-Pro-Rostislav.local>
2021-05-28 00:17:34 +0300

    0.6.0 Add .d.ts and @types/mysql dep

---
+++ lib/index.d.ts
@@ -0,0 +1,333 @@
+import {
+  Connection,
+  ConnectionConfig,
+  ConnectionOptions,
+  EscapeFunctions,
+  FieldInfo,
+  OkPacket,
+  QueryOptions,
+} from "mysql"
+
+export const version: string
+export function createConnector(options: ConnectionConfig): ConnectorInstance
+
+declare interface ConnectorInstance
+  extends SelectExtPrototype,
+    UpdateExtPrototype,
+    InsertExpPrototype,
+    DeleteExpPrototype {
+  options: ConnectionConfig | null
+  current: Connection | null
+
+  connect(): Promise<Connection>
+  close(immediately?: boolean): Promise<void>
+  destroy(): void
+  query: QueryFunction
+  fetch: FetchFunction
+  changeUser(options: ConnectionOptions): Promise<Connection>
+  pause(): void
+  resume(): void
+  readonly threadId: number | null
+  readonly state: string | null
+  ping(): Promise<void>
+  escape: EscapeFunctions["escape"]
+  escapeId: EscapeFunctions["escapeId"]
+  clone(): ConnectorInstance
+}
+
+declare interface QueryFunction {
+  (sql: string, values: QueryValues, options: QueryOptions): Promise<Query>
+  (sql: string, options: QueryOptions): Promise<Query>
+}
+
+declare interface FetchOptions extends QueryOptions {
+  one?: boolean
+  index?: number
+}
+
+declare interface FetchFunction {
+  (sql: string, values: QueryValues, options: FetchOptions): Promise<any>
+  (sql: string, options: FetchOptions): Promise<any>
+}
+
+declare type QueryValues = any[]
+
+declare interface Query {
+  results?: OkPacket
+  fields?: FieldInfo[]
+}
+
+declare interface DeleteExpPrototype {
+  delete: DeleteInstance["delete"]
+}
+
+declare interface DeleteInstance
+  extends FlagPart<DeleteInstance, "LOW_PRIORITY" | "QUICK" | "IGNORE">,
+    WherePart<DeleteInstance>,
+    OrderByPart<DeleteInstance>,
+    LimitPartWithOneParameter<DeleteInstance>,
+    QueryPrototype,
+    FetchPrototype<OkPacket>,
+    ClonePrototype<DeleteInstance> {
+  delete: () => DeleteInstance
+  from: (
+    ...tblNames: Array<{ [key: string]: string } | string>
+  ) => DeleteInstance
+  using: (...tableReferences: TableReferences[]) => DeleteInstance
+  toSqlString: () => string
+}
+
+declare interface InsertExpPrototype {
+  insert: InsertInstance["insert"]
+}
+
+declare interface InsertInstance
+  extends FlagPart<
+      InsertInstance,
+      "LOW_PRIORITY" | "DELAYED" | "HIGH_PRIORITY" | "IGNORE"
+    >,
+    QueryPrototype,
+    FetchPrototype<OkPacket>,
+    ClonePrototype<InsertInstance> {
+  insert: (...values: InsertValue[]) => InsertInstance
+  into: (tblName: string) => InsertInstance
+  cols: (...cols: string[]) => InsertInstance
+  as: (rowAlias: string, colAliases?: string[]) => InsertInstance
+  onDuplicateKeyUpdate: (...assigmentList: AssigmentItem[]) => InsertInstance
+  set: (...assigmentList: AssigmentItem[]) => InsertInstance
+  select: (instance: string | SelectInstance) => InsertInstance
+  table: (tblName: string) => InsertInstance
+  toSqlString: () => string
+}
+
+declare type InsertValue =
+  | Value[]
+  | string
+  | { row: InsertValue }
+  | { [key: string]: Value }
+
+declare interface UpdateExtPrototype {
+  update: UpdateInstance["update"]
+}
+
+declare interface UpdateInstance
+  extends FlagPart<UpdateInstance, "LOW_PRIORITY" | "IGNORE">,
+    WherePart<UpdateInstance>,
+    OrderByPart<UpdateInstance>,
+    LimitPartWithOneParameter<UpdateInstance>,
+    QueryPrototype,
+    FetchPrototype<OkPacket>,
+    ClonePrototype<UpdateInstance> {
+  update(...tableReferences: TableReferences[]): UpdateInstance
+  set(...assigmentList: AssigmentItem[]): UpdateInstance
+  toSqlString(): string
+}
+
+declare type AssigmentItem = { [key: string]: Value } | string
+
+declare interface SelectExtPrototype {
+  select: SelectInstance["select"]
+}
+
+declare interface SelectInstance
+  extends FlagPart<
+      SelectInstance,
+      | "ALL"
+      | "DISTINCT"
+      | "DISTINCTROW"
+      | "HIGH_PRIORITY"
+      | "STRAIGHT_JOIN"
+      | "SQL_SMALL_RESULT"
+      | "SQL_BIG_RESULT"
+      | "SQL_BUFFER_RESULT"
+      | "SQL_NO_CACHE"
+      | "SQL_CALC_FOUND_ROWS"
+    >,
+    IntoPart<SelectInstance>,
+    WherePart<SelectInstance>,
+    GroupByPart<SelectInstance>,
+    HavingPart<SelectInstance>,
+    OrderByPart<SelectInstance>,
+    LimitPart<SelectInstance>,
+    LockPart<SelectInstance>,
+    QueryPrototype,
+    FetchPrototype,
+    ClonePrototype<SelectInstance> {
+  connector: ConnectorInstance
+
+  select(...selectExpressions: Expression[]): SelectInstance
+  from(...tableReferences: TableReferences[]): SelectInstance
+  count(
+    list: Array<{ distinct: string } | string>,
+    options: FetchOptions
+  ): SelectInstance
+  toSqlString(): string
+}
+
+declare interface ClonePrototype<T> {
+  clone(): T
+}
+
+declare interface QueryPrototype {
+  query(values: QueryValues, options: QueryOptions): Promise<Query>
+  query(options: QueryOptions): Promise<Query>
+}
+
+declare interface FetchPrototype<T = any> {
+  fetch(values: QueryValues, options: FetchOptions): Promise<T>
+  fetch(options: FetchOptions): Promise<T>
+}
+
+declare type LockPart<T> = {
+  lockInShare: () => T
+  lockFor: (options: {
+    mode: "update" | "share"
+    of: string[]
+    flag: "nowait" | "skip locked"
+  }) => T
+}
+
+declare type LimitPart<T> = {
+  limit: (offset: number, length: number) => T
+} & LimitPartWithOneParameter<T>
+
+declare type LimitPartWithOneParameter<T> = {
+  limit: (length: number) => T
+}
+
+declare type OrderByPart<T> = {
+  orderBy: (...args: OrderByArg[]) => T
+  orderByWithRollup: (...args: OrderByArg[]) => T
+}
+
+declare type OrderByArg =
+  | [Expression, "asc" | "desc"]
+  | { [key: string]: "asc" | "desc" }
+  | string
+
+declare type HavingPart<T> = {
+  having: (...conditions: SearchCondition[]) => T
+}
+
+declare type GroupByPart<T> = {
+  groupBy: (...args: GroupByArg[]) => T
+  groupByWithRollup: (...args: GroupByArg[]) => T
+}
+
+declare type GroupByArg = number | Expression
+
+declare type WherePart<T> = {
+  where: (...whereConditions: SearchCondition[]) => T
+}
+
+declare type TableReferences = JoinExpression | Expression | TableReferences[]
+
+declare type JoinExpression = {
+  join: Expression
+  prefix?: string
+  using?: string[]
+  on?: SearchCondition
+}
+
+declare type CondLeftRight =
+  | string
+  | {
+      [key: string]:
+        | ConstantExpression
+        | ConstantEscapeValue
+        | ConstantEscapeId
+        | ToSql
+        | {
+            $?: {
+              op: string
+              value: Value
+            }
+            $eq?: Value
+            $neq?: Value
+            $lt?: Value
+            $lte?: Value
+            $gt?: Value
+            $gte?: Value
+            $nseq?: Value
+            $btw?: [Value, Value]
+            $nbtw?: [Value, Value]
+            $in?: Value
+            $nin?: Value
+            $is?: null | string
+            $isnt?: null | string
+            $lk?: Value
+            $nlk?: Value
+          }
+    }
+  | CondLeftRight[]
+
+declare type SearchCondition =
+  | string
+  | {
+      $or?: CondLeftRight
+      $and?: CondLeftRight
+      $xor?: CondLeftRight
+      $?: {
+        left: Expression
+        op: string
+        right: Value
+      }
+    }
+  | CondLeftRight
+  | SearchCondition[]
+
+declare type IntoPartOptions = {
+  position: "afterExpression" | "afterLimit" | "afterLock"
+  dumpFile?: string
+  vars?: string[]
+  outFile?: string
+  charSet?: string
+  fieldsTerminatedBy?: string
+  fieldsEnclosedBy?: string
+  fieldsEscapedBy?: string
+  columnsTerminatedBy?: string
+  columnsEnclosedBy?: string
+  columnsEscapedBy?: string
+  linesStartingBy?: string
+  linesTerminatedBy?: string
+}
+declare type IntoPart<T> = {
+  into: (options: IntoPartOptions) => T
+}
+
+declare type FlagPart<T, K> = {
+  flag: (name: K, show?: boolean) => T
+}
+
+declare type ToSql = SelectInstance
+
+declare type ConstantExpression = { $: any }
+declare type ConstantEscapeValue = {
+  $e: any
+  stringifyObjects?: boolean
+  timeZone?: string
+}
+declare type ConstantEscapeId = { $eId: any; forbidQualified?: boolean }
+
+declare type Value =
+  | null
+  | string
+  | number
+  | Date
+  | Value[]
+  | ConstantExpression
+  | ToSql
+  | ConstantEscapeValue
+  | ConstantEscapeId
+
+declare type Expression =
+  | null
+  | string
+  | number
+  | Date
+  | Expression[]
+  | ConstantExpression
+  | ToSql
+  | ConstantEscapeValue
+  | ConstantEscapeId
+  | { [key: string]: Expression }
--- test/index.js
+++ test/index.js
@@ -9,14 +9,4 @@ const db = createConnector({
 
 db.connect().then(async function () {
   console.log("Connected")
-  const query = db
-    .delete()
-    .from({ t: "test" })
-    .from({ t2: "test2" })
-    .using("t", { join: "t2", on: { "t2.a_id": { $eId: "t1.id" } } })
-    .where({ "t2.id": { $gt: 10 } })
-    .orderBy({ a: "desc" })
-    .limit(50)
-  console.log(query.toSqlString())
-  // delete from `test` `t`,`test2` `t2` using `t` inner join `t2` on `t2`.`a_id`=`t1`.`id` where `t2`.`id`>10 order by `a` desc limit 50
 })
