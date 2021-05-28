f306cb3feccb32719beadc29b2dff9608af9eba1
Ростислав Профит <rostislavprofit@MacBook-Pro-Rostislav.local>
2021-05-28 15:38:19 +0300

    0.8.0 clone ext

--- test/index.js
+++ test/index.js
@@ -7,13 +7,35 @@ const db = createConnector({
   database: "test",
 })
 
-const query = db.cache(({ col, id }) =>
-  db.select(col).from("user").where({ id })
-)
+const query = db
+  .cache(
+    ["id", "col", "val", "tbl", "al", "file", "o"],
+    ({ id, col, tbl, al, file, o }) =>
+      db
+        .select(col)
+        .from({ $: `${tbl} ${al}` })
+        .where({ id })
+        .groupBy(col)
+        .orderBy([col, o])
+        .into({ dumpFile: file, position: "afterLock" })
+  )
+  .expand((instance, { col, val }) =>
+    instance.where({ $: { left: col, op: ">", right: val } })
+  )
 
 db.connect().then(async function () {
   console.log("Connected")
 
-  console.log(query.toSqlString())
-  // select * from `a` union all select * from `b` union (select * from `c` union select 1) union table `test`
+  console.log(
+    query.toSqlString({
+      id: { $e: 5 },
+      col: { $eId: "test" },
+      val: { $: 10 },
+      tbl: { $eId: "user" },
+      al: { $eId: "u" },
+      file: { $e: "test.txt" },
+      o: { $: "desc" },
+    })
+  )
+  // select `test` from `user` `u` where `id`=5 and `test`>10 group by `test` order by `test` desc into dumpfile 'test.txt'
 })
